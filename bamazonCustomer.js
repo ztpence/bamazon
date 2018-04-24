60251269a8dd54ee519b48a3f5296a50a9f440b7
Zane P <ztpence@gmail.com>
2018-04-23 21:48:40 -0700

    added functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,8 @@ var inquirer = require('inquirer');
 var mysql = require('mysql');
 require('console.table')
 
+var id;
+
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -9,11 +11,33 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-//create connection function
+//create connection function err to handle errors upfront
 
-//create function for merchandise on sale with connection query
+connection.connect(function(err){
+    if (err) throw err;
+    console.log(connection.threadID + 'id connected ');
+    productForSale();
+});
+
+//create function for product on sale with connection query
+
+function productForSale() {
+    connection.query('SELECT * FROM products', function(err, res){
+        if (err) throw err;
+        else{
+            console.log('Products for Sale: ');
+            for (var i = 0; i < res.length; i++){
+                console.log(res[i].id + '. ' + res[i].product_name + '| ' + res[i].price);
+            }
+          inquireUser();
+        }
+    });
+}
 
 //neeed to inquirer customer with questions
+function inquireUser() {
+
+}
 
 //then take in customer info with and run promise function
 
